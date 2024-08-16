ee8091b7563f3a850ca61c99c6a8a7e161fc9bb9
Artyom <ArtKuroc@yandex.ru>
2024-08-16 13:02:23 +0300

    16.08.2024

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Введите Ваше имя: ")
+current_year = 2024
+date_of_birth = (input("В каком году Вы родились: "))
+age = current_year - int(date_of_birth)
+print("Здравствуйте,", name)
+print("В этом году Вам ", age, "год")
+print('Привет, я очень рад тебя здесь видеть!'.replace('Привет','Здравствуй'))
