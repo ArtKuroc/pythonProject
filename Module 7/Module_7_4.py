3b72a19c751603899ac9d02f49180eaa2d49a961
Артём <ArtKuroc@yandex.ru>
2024-11-05 19:49:01 +0300

    16.08.2024

---
+++ Module 7/Module_7_4.py
@@ -0,0 +1,17 @@
+team1_num = 5
+print('В команде Мастера кода участников: %s !' % team1_num)
+team1_num2 = 6
+print('Итого сегодня в командаж участников: %s и %s!' % (5, 6))
+# Использование format():
+score_2 = 42
+print('Команда Волшебники данных решила задач: {}'.format(score_2))
+team1_time = 18015.2
+print('Волшебники данных решили задачи за {} c !'.format(team1_time))
+# Использование f-строк:
+score_1 = 40
+print(f'Команды решили {score_1} и {score_2} задач.')
+challenge_result = 'победа команды Мастера кода!'
+print(f'Результат битвы: {challenge_result}')
+tasks_total = 82
+time_avg = 350.4
+print(f'Сегодня было решено {tasks_total} задачи, в средние по {time_avg} секунды на задачу')
