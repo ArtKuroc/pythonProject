d102c13aa9e42d910e184ce3105113d7b7564964
Артём <ArtKuroc@yandex.ru>
2024-11-13 13:34:59 +0300

    16.08.2024

---
+++ Module 9/Module_9_2.py
@@ -0,0 +1,10 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(s) for s in first_strings if len(s) > 4]
+second_result = [(s, y) for s in first_strings for y in second_strings if len(s) == len(y)]
+third_result = {s: len(s) for s in first_strings + second_strings if len(s) % 2 == 0}
+
+print(first_result)
+print(second_result)
+print(third_result)
